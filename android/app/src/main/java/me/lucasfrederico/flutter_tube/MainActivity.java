60b21e5e220e6f503a9e30d062e0c66b6d984462
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-01-28 19:25:44 -0300

    feat: criando e configurando o projeto

---
+++ android/app/src/main/java/me/lucasfrederico/flutter_tube/MainActivity.java
@@ -0,0 +1,6 @@
+package me.lucasfrederico.flutter_tube;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/api.dart
@@ -0,0 +1,1 @@
+const API_KEY = 'AIzaSyAbDOEhk3f4iNK7YAYJnXXae6BBGiyifyw';

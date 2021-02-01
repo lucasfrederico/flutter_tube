494ca193bb1f5bda971ef7c956a4329f2a8db59b
Lucas Rodrigo Frederico <55282018+lucasfrederico@users.noreply.github.com>
2021-02-01 12:24:48 -0300

    feat: criando o layout da tela principal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/youtube_logo.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
